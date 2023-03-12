ca97e7f3b1c6752afbf7b39cc4dde6e1f8e448c5
Michle99 <sammiyonyx@gmail.com>
2023-03-12 14:18:43 -0400

    updated the folder structure and clean up

--- App.js
+++ App.js
@@ -7,6 +7,7 @@ import TopPickScreen from './screens/TopPickScreen';
 import MessageScreen from './screens/MessageScreen';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import IonIcon from 'react-native-vector-icons/Ionicons';
+import { FontAwesome } from '@expo/vector-icons'; 
 
 const Tab = createBottomTabNavigator();
 
@@ -28,7 +29,7 @@ export default function App() {
           options={{
             tabBarLabel: 'Home',
             tabBarIcon: ({ focused }) => (
-              <MaterialCommunityIcons name='fire' focused={focused} size={30} color="pink" />
+              <MaterialCommunityIcons name='fire' focused={focused} size={30} />
             ),
           }}
         />
@@ -38,7 +39,7 @@ export default function App() {
           options={{
             tabBarLabel: 'Profile',
             tabBarIcon: ({ focused }) => (
-              <MaterialCommunityIcons name='account' focused={focused} size={30} color="blue" />
+              <MaterialCommunityIcons name='account' focused={focused} size={30} />
             ),
           }}
         />
@@ -48,7 +49,7 @@ export default function App() {
           options={{
             tabBarLabel: 'TopPick',
             tabBarIcon: ({ focused }) => (
-              <IonIcon name='star' focused={focused} size={30} color="red" />
+              <FontAwesome name='diamond' focused={focused} size={30}/>
             ),
           }}
         />
@@ -58,7 +59,7 @@ export default function App() {
           options={{
             tabBarLabel: 'Home',
             tabBarIcon: ({ focused }) => (
-              <MaterialCommunityIcons name='message' focused={focused} size={30} color="green" />
+              <FontAwesome name='commenting-o' focused={focused} size={30} />
             ),
           }}
         />
@@ -66,15 +67,3 @@ export default function App() {
      </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  home: {
-    backgroundColor: 'red',
-  },
-});
